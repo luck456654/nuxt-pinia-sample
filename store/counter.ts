3aeba70ec3f7de58535131f23d456d6df43cb548
Вадим <44877772+luck456654@users.noreply.github.com>
2024-05-04 19:27:19 +0300

    Update counter.ts

--- store/counter.ts
+++ store/counter.ts
@@ -1,19 +1,16 @@
 import { defineStore } from 'pinia'
 
-/** counterストア */
+
 export const useCounterStore = defineStore('counter', {
   state: () => ({
-    /** 件数 */
+   
     count: 0,
   }),
   getters: {
-    /** countを2倍 */
+    
     double: (state) => state.count * 2,
   },
   actions: {
-    /**
-     * countに1を加算
-     */
     increment() {
       this.count++
     },
